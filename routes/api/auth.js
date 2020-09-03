7d646766b9aea0e415c7e06beea7e2e600453d0a
dev <dev@kierans-portfolio.attlocal.net>
2020-09-03 16:15:52 -0500

    Add logout methods

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -57,8 +57,12 @@ router.post('/login', function(req, res, next) {
   })(req, res, next);
 });
 
-router.get('/logout', function(req, res){
+router.delete('/logout', function(req, res){
   req.logout();
+  if(!req.session.passport.user){
+    return res.json({success: 'true'});
+  }
+  return res.json({success: 'false'});
 });
 
 module.exports = router;
