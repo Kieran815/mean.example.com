7c34e0a46db355d66d9b7a99b839544f24793b6c
dev <dev@kierans-portfolio.attlocal.net>
2020-09-03 12:24:04 -0500

    added retrieve single user

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,4 +12,16 @@ router.get('/', function(req, res, next) {
   });
 });
 
+// GET request, User ID
+router.get('/:userId', function(req,res){
+  
+  var userId = req.params.userId;
+   Users.findOne({'_id':userId}, function(err, user){
+     if(err){
+      return res.json({'success':false, 'error': err});
+    }
+     return res.json({'success':true, 'user': user});
+   });
+ });
+
 module.exports = router;
