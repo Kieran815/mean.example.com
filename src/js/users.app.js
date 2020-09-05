b042615e0cce1ce19841a4ffb332e5ad07df495c
dev <dev@kierans-portfolio.attlocal.net>
2020-09-05 14:28:27 -0500

    added deleteUser method

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -280,8 +280,8 @@ var usersApp = (function() {
 
           <div class="text-center">
             <br>
-            <a class="btn btn-lg btn-danger text-white">
-              Yes delete ${data.user.username}
+            <a onclick="usersApp.deleteUser('${data.user._id}');" class="btn btn-lg btn-danger text-white">
+              Yes, delete ${data.user.username}
             </a>
           </div>
 
@@ -291,6 +291,31 @@ var usersApp = (function() {
       app.innerHTML = card;
     }
   }  
+
+  function deleteUser(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('DELETE', uri);
+
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+
+    xhr.send();
+
+    xhr.onload = function(){
+      let data = JSON.parse(xhr.response);
+      if(data.success === true){
+        window.location.hash = '#';
+      }else{
+        alert('Unknown error, the user could not be deleted');
+      }
+
+    }
+
+  }
 
   return {
     load: function(){
@@ -319,6 +344,9 @@ var usersApp = (function() {
           viewUsers();
           break;
       }
+    },
+    deleteUser: function(id) {
+      deleteUser(id);
     }
   }
   
